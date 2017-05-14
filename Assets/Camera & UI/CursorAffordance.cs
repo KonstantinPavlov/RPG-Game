f3005aebca83689061d7247b09a4711db0288e8b
Konstantin Pavlov <konstpavlovsdr@gmail.com>
2017-05-14 15:27:22 +0300

    add cursor implementation

--- Assets/Camera & UI/CursorAffordance.cs
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CursorAffordance : MonoBehaviour {
+    
+    [SerializeField]
+    Vector2 cursorHotspot = new Vector2(0,0);
+    [SerializeField]
+    Texture2D walkCursor = null;
+    [SerializeField]
+    Texture2D attackCursor = null;
+    [SerializeField]
+    Texture2D endWalkCursor = null;
+    [SerializeField]
+    Texture2D unusedCursor = null;
 
-     private CameraRaycaster cameraRaycaster;
+    private CameraRaycaster cameraRaycaster;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +23,23 @@ public class CursorAffordance : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        if (Input.GetKeyDown(KeyCode.Mouse0)){
-            print(cameraRaycaster.layerHit);
-        }        
-	}
+	void Update () {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(attackCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(endWalkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(unusedCursor, cursorHotspot, CursorMode.Auto);
+                Debug.LogError("Unexpeted Layer!");
+                return;
+        }            
+       
+    }
 }
