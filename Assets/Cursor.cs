a37653d70fe2fc6836dde7a1b8c35f2d608db466
Konstantin Pavlov <konstpavlovsdr@gmail.com>
2017-05-14 10:58:05 +0300

    add Cursor.class - test for CameraRaycater

---
+++ Assets/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+     private CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            print(cameraRaycaster.layerHit);
+        }        
+	}
+}
