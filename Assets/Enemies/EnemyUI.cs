745e1b3c6250ecd892118004f14f8bb7501d4edf
Konstantin Pavlov <konstpavlovsdr@gmail.com>
2017-05-25 00:15:39 +0300

    Fix enemy UiI strange rotation

--- Assets/Enemies/EnemyUI.cs
+++ Assets/Enemies/EnemyUI.cs
@@ -23,6 +23,6 @@ public class EnemyUI : MonoBehaviour {
     void LateUpdate()
     {
         transform.LookAt(cameraToLookAt.transform);
-        transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
+        //transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
     }
 }
