758c88db217a0592042475e27545b5d46e6b4865
Konstantin Pavlov <konstpavlovsdr@gmail.com>
2017-05-24 23:59:29 +0300

    Add Nav mesh and Enemies pathfinding to player

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
@@ -10,11 +11,35 @@ public class Enemy : MonoBehaviour {
     [SerializeField]
     float maxHealthPoints = 100f;
 
-    public float healthAsPercentage
-    {
+    [SerializeField]
+    float attackRadius = 1f;
+
+    AICharacterControl aiCharacterControl = null;
+
+    GameObject player=null;
+
+    public float healthAsPercentage {
         get
         {
             return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    private void Start() {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+
+    }
+
+    private void Update() {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if(distanceToPlayer <= attackRadius) {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 
